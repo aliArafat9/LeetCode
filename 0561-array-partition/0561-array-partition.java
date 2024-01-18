d6d81bb9cfbfab17995bb5b17832a56161f7c929
Arafat <94069423+aliArafat9@users.noreply.github.com>
2024-01-19 01:55:00 +0530

    Time: 14 ms (31.01%), Space: 46.8 MB (14.05%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int arrayPairSum(int[] nums) {
+        int res = 0;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i = i + 2) {
+            res = res + Math.min(nums[i], nums[i + 1]);
+        }
+        return res;
+    }
+}
