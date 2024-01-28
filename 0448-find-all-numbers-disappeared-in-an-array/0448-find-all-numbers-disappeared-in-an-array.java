5142d38acf1d52ee86699397c064bbdaaba8d28f
Arafat <94069423+aliArafat9@users.noreply.github.com>
2024-01-28 16:14:44 +0530

    Time: 20 ms (13.49%), Space: 55.4 MB (13.60%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        //** we will add very element of nums in the set so that the duplicates are removed.
+        for (int num : nums)
+            set.add(num);
+
+        //?? then we will add the elements in the list which are not present in the set
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i))
+                ans.add(i);
+        }
+
+        return ans;
+    }
+}
